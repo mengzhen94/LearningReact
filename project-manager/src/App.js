f41a6f324aff8b8b61d9e75d74976defb130861a
Mengzhen <gao.mengzhen94@gmail.com>
2017-07-28 16:13:59 -0400

    init react

---
+++ project-manager/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      // We has only one element, it means there is not another div here
+      <div className="App">
+        My App
+      </div>
+    );
+  }
+}
+
+export default App;
